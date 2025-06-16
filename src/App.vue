adfc5f9782a29916d88ee51dc70b39cdffa1b057
homeScl <419863966@qq.com>
2025-06-16 15:19:51 +0800

    主进程对话框练习

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
     <!-- <Hello /> -->
     <!-- <WindowOps /> -->
-    <Communication />
+    <!-- <Communication /> -->
+     <Dialog />
 </template>
 <script setup lang="ts">
+import Dialog from './components/Dialog.vue';
 import Communication from './components/Communication.vue';
 import Hello from './components/Hello.vue';
 import WindowOps from './components/WindowOps.vue';
---
+++ src/components/Dialog.vue
@@ -0,0 +1,29 @@
+<<template>
+
+    <div>
+        <h2>主进程对话框练习</h2>
+        <button @click="showDialog">打开对话框</button>
+    </div>
+
+</template>
+
+<script setup lang="ts">
+import { ref, reactive } from "vue"
+const {dialog } = require("electron")
+
+// 验证无效
+const showDialog = ()=>{
+    console.log(11)
+    dialog.showOpenDialog({
+        title:"打开文件",
+        buttonLabel:"打开",
+        properties:["openFile"]
+    }).then(result=>{
+        console.log(result)
+    })
+}
+</script>
+
+<style scoped>
+
+</style>
